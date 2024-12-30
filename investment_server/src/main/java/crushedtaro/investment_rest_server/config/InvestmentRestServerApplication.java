63de60231b3bf6bed57ec487981541b899ceb5fb
crushed-taro <97sinsang@gmail.com>
2024-12-31 01:36:11 +0900

    EntityScan, EnableSpaRepositories 경로 추가.

--- investment_server/src/main/java/crushedtaro/investment_rest_server/config/InvestmentRestServerApplication.java
+++ investment_server/src/main/java/crushedtaro/investment_rest_server/config/InvestmentRestServerApplication.java
@@ -2,10 +2,14 @@ package crushedtaro.investment_rest_server.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan("crushedtaro.investment_rest_server")
+@EntityScan(basePackages = "crushedtaro.investment_rest_server")
+@EnableJpaRepositories(basePackages = "crushedtaro.investment_rest_server")
 public class InvestmentRestServerApplication {
 
 	public static void main(String[] args) {
